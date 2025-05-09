5bae524eedc030b2cdd909a728a9f9ea426c107f
aswathi-48 <aswathik482@gmail.com>
2025-05-09 10:00:25 +0530

    Home changes

--- dessert-store/src/Component/Home/Home.jsx
+++ dessert-store/src/Component/Home/Home.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Home.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 const images = [
-  '../../../public/Images/joyful-vT5xrj3z1OE-unsplash.jpg',
-  '../../../public/Images/allen-rad-JBIK4QZOFfc-unsplash.jpg',
-  '../../../public/Images/joyful-vT5xrj3z1OE-unsplash.jpg'
+  '/Images/joyful-vT5xrj3z1OE-unsplash.jpg',
+  '/Images/allen-rad-JBIK4QZOFfc-unsplash.jpg',
+  '/Images/dessert.jpg'
 ];
 
 const Home = () => {
+  const sliderRef = useRef(null);
+
+  const scrollLeft = () => {
+    sliderRef.current.scrollBy({ left: -sliderRef.current.offsetWidth, behavior: 'smooth' });
+  };
+
+  const scrollRight = () => {
+    sliderRef.current.scrollBy({ left: sliderRef.current.offsetWidth, behavior: 'smooth' });
+  };
+
   return (
-    <div className="slider-container">
-      {images.map((src, index) => (
-        <div className="slide" key={index}>
-          <img src={src} alt={`slide-${index}`} />
-        </div>
-      ))}
+    <div className="slider-wrapper">
+      <button className="arrow left" onClick={scrollLeft}>
+        <FaChevronLeft />
+      </button>
+      <div className="slider-container" ref={sliderRef}>
+        {images.map((src, index) => (
+          <div className="slide" key={index}>
+            <img src={src} alt={`slide-${index}`} />
+          </div>
+        ))}
+      </div>
+      <button className="arrow right" onClick={scrollRight}>
+        <FaChevronRight />
+      </button>
     </div>
   );
 };
