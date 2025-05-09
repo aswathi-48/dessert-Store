006d05e0c841b14aee08f2f23c16fcce735ca291
aswathi-48 <aswathik482@gmail.com>
2025-05-09 09:47:55 +0530

    app.jsx

--- dessert-store/src/App.jsx
+++ dessert-store/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Navbar from './Component/Navbar/Navbar'
 
 
 function App() {
 
   return (
     <>
+       <Navbar />
        <Router>
        <Routes>
-         
+     
        </Routes>
        </Router>
      
