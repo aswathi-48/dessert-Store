5180835c9210985d50b056365e3838ef7c2b1a2e
aswathi-48 <aswathik482@gmail.com>
2025-05-09 09:39:41 +0530

    browserroute

--- dessert-store/src/App.jsx
+++ dessert-store/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      
+       <Router>
+       <Routes>
+        
+       </Routes>
+       </Router>
+     
     </>
   )
 }
