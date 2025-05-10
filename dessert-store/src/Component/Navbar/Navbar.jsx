603157d77f74da829b49c2920f12a5a0aafaf939
aswathi-48 <aswathik482@gmail.com>
2025-05-10 14:47:09 +0530

    nav

--- dessert-store/src/Component/Navbar/Navbar.jsx
+++ dessert-store/src/Component/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <li><a href="#home">Home</a></li>
         <li><a href="#menu">Menu</a></li>
         <li><a href="#specials">Specials</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="/about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
       <button className="mobile-menu-icon" onClick={toggleMenu}>
