82d89ceda10262433e9d52c0242662a8875eecb6
aswathi-48 <aswathik482@gmail.com>
2025-05-10 12:23:31 +0530

    change

--- dessert-store/src/Component/Home/Home.jsx
+++ dessert-store/src/Component/Home/Home.jsx
@@ -106,25 +106,13 @@ const Home = () => {
   return (
     <div className="home-page">
       {/* Hero Section */}
-      {/* <section className="hero">
+      <section className="hero">
         <h1>🍰 Welcome to SweetTreats</h1>
         <p>Delicious desserts made with love. Order your favorite now!</p>
         <button className="order-btn">Explore Desserts</button>
-      </section> */}
+      </section>
 
-      {/* Video Section */}
-<section className="video-section">
-  <video autoPlay loop muted className="bg-video">
-    <source src="/Videos/video.mp4" type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-  <div className="video-overlay">
-    <h2 data-aos="zoom-in">Experience the Art of Desserts</h2>
-    <p data-aos="fade-up">Handcrafted sweets with love and passion – every bite tells a story.</p>
-    <button className="order-btn" data-aos="fade-up">Watch More</button>
-  </div>
-</section>
-
+     
 
       {/* Scrolling Images */}
       <section className="marquee-section">
