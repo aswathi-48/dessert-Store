eb3682625b6f7b2bddc820eab23b3c697baa6941
aswathi-48 <aswathik482@gmail.com>
2025-05-11 22:02:51 +0530

    outlet

--- dessert-store/src/App.jsx
+++ dessert-store/src/App.jsx
@@ -1,27 +1,50 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Navbar from './Component/Navbar/Navbar'
-import Home from './Component/Home/Home'
-import About from './Component/About/About'
-import Menu from './Component/Menu/Menu'
-import Footer from './Component/footer/Footer'
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+// import Navbar from './Component/Navbar/Navbar'
+// import Home from './Component/Home/Home'
+// import About from './Component/About/About'
+// import Menu from './Component/Menu/Menu'
+// import Footer from './Component/footer/Footer'
 
+
+// function App() {
+
+//   return (
+//     <Router>
+//     <Navbar />
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/about" element={<About />} />
+//       <Route path="/menu" element={<Menu />} />
+
+
+//     </Routes>
+//     <Footer/>
+//   </Router>
+
+//   )
+// }
+
+// export default App
+
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './Component/Home/Home';
+import About from './Component/About/About';
+import Menu from './Component/Menu/Menu';
+import Layout from './Layout';
 
 function App() {
-
   return (
     <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/menu" element={<Menu />} />
-
-
-    </Routes>
-    <Footer/>
-  </Router>
-
-  )
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} /> {/* path="/" */}
+          <Route path="about" element={<About />} />
+          <Route path="menu" element={<Menu />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
