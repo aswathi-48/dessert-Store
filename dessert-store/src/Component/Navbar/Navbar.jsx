13f54d1c1ef0393bc892bdc4278af36fdf9ba76b
aswathi-48 <aswathik482@gmail.com>
2025-05-11 22:06:46 +0530

    change to navbar

--- dessert-store/src/Component/Navbar/Navbar.jsx
+++ dessert-store/src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom'; // <-- Import Link
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -13,11 +14,11 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="logo">Dessert Delight</div>
       <ul className={isMobile ? 'nav-links-mobile' : 'nav-links'} onClick={() => setIsMobile(false)}>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#menu">Menu</a></li>
-        <li><a href="#specials">Specials</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><a href="#specials">Specials</a></li> 
+        <li><Link to="/about">About</Link></li>
+        <li><a href="#contact">Contact</a></li> 
       </ul>
       <button className="mobile-menu-icon" onClick={toggleMenu}>
         {isMobile ? <FaTimes /> : <FaBars />}
