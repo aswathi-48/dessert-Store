4ced20cd88a55ca64b39efd13cacbd0449946071
aswathi-48 <aswathik482@gmail.com>
2025-05-13 16:46:45 +0530

    spcl

--- dessert-store/src/App.jsx
+++ dessert-store/src/App.jsx
@@ -6,6 +6,7 @@ import About from './Component/About/About';
 import Menu from './Component/Menu/Menu';
 import Layout from './Layout';
 import Contact from './Component/Contact/Contact';
+import Specials from './Component/Specials/Specials';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="menu" element={<Menu />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="specials" element={<Specials />} />
 
         </Route>
       </Routes>
