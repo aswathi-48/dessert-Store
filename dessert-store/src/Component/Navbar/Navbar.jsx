bd604634e789b069085f883ec30d6ae933c50f19
aswathi-48 <aswathik482@gmail.com>
2025-05-13 12:42:36 +0530

    change

--- dessert-store/src/Component/Navbar/Navbar.jsx
+++ dessert-store/src/Component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom'; // <-- Import Link
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
