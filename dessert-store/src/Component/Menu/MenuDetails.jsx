71a368930cdbc2d10e76f0c522c2a55f4e70ac41
aswathi-48 <aswathik482@gmail.com>
2025-05-19 12:45:36 +0530

    unwanted data removed

--- dessert-store/src/Component/Menu/MenuDetails.jsx
+++ dessert-store/src/Component/Menu/MenuDetails.jsx
@@ -52,34 +52,6 @@ const MenuDetails = () => {
                 <h2>{dessert.name}</h2>
                 <p className="price">{dessert.price}</p>
                 <p className="description">{dessert.description}</p>
-
-                {dessert.ingredients && (
-                    <div className="ingredients">
-                        <h4>Ingredients</h4>
-                        <ul>
-                            {dessert.ingredients.map((ing, index) => (
-                                <li key={index}>{ing}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
-
-                {dessert.allergy_info && (
-                    <p className="allergy-info"><strong>Allergy Info:</strong> {dessert.allergy_info}</p>
-                )}
-
-                {dessert.nutrition && (
-                    <div className="nutrition">
-                        <h4>Nutrition Facts</h4>
-                        <ul>
-                            <li><strong>Calories:</strong> {dessert.nutrition.calories}</li>
-                            <li><strong>Fat:</strong> {dessert.nutrition.fat}</li>
-                            <li><strong>Carbs:</strong> {dessert.nutrition.carbs}</li>
-                            <li><strong>Protein:</strong> {dessert.nutrition.protein}</li>
-                        </ul>
-                    </div>
-                )}
-
                 {dessert.reviews && dessert.reviews.length > 0 && (
                     <div className="reviews">
                         <h4>Customer Reviews</h4>
