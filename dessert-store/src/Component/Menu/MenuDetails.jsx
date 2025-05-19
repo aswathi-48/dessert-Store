a790cebd5eff39569a7b64d263408caa7086cb08
aswathi-48 <aswathik482@gmail.com>
2025-05-19 12:28:49 +0530

    chnage to perv

--- dessert-store/src/Component/Menu/MenuDetails.jsx
+++ dessert-store/src/Component/Menu/MenuDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import dessertItems from '../../data/desserts.json';
-import './Menudetail.css';
+import './Menudetail.css'
 
 const MenuDetails = () => {
     const { name } = useParams();
@@ -16,45 +16,6 @@ const MenuDetails = () => {
             <h2>{dessert.name}</h2>
             <p className="price">{dessert.price}</p>
             <p className="description">{dessert.description}</p>
-
-            {dessert.ingredients && (
-                <div className="ingredients">
-                    <h4>Ingredients</h4>
-                    <ul>
-                        {dessert.ingredients.map((ing, index) => (
-                            <li key={index}>{ing}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-
-            {dessert.allergy_info && (
-                <p className="allergy-info"><strong>Allergy Info:</strong> {dessert.allergy_info}</p>
-            )}
-
-            {dessert.nutrition && (
-                <div className="nutrition">
-                    <h4>Nutrition Facts</h4>
-                    <ul>
-                        <li><strong>Calories:</strong> {dessert.nutrition.calories}</li>
-                        <li><strong>Fat:</strong> {dessert.nutrition.fat}</li>
-                        <li><strong>Carbs:</strong> {dessert.nutrition.carbs}</li>
-                        <li><strong>Protein:</strong> {dessert.nutrition.protein}</li>
-                    </ul>
-                </div>
-            )}
-
-            {dessert.reviews && dessert.reviews.length > 0 && (
-                <div className="reviews">
-                    <h4>Customer Reviews</h4>
-                    {dessert.reviews.map((review, index) => (
-                        <div key={index} className="review">
-                            <p><strong>{review.user}</strong>: {review.comment}</p>
-                            <p>Rating: {review.rating}/5</p>
-                        </div>
-                    ))}
-                </div>
-            )}
         </div>
     );
 };
