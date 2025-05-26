4142d281bbb3c559ef382972fe7eaef560499777
aswathi-48 <aswathik482@gmail.com>
2025-05-26 12:20:24 +0530

    contact

--- dessert-store/src/Component/Contact/Contact.jsx
+++ dessert-store/src/Component/Contact/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
           <p><strong>Address:</strong> 123 Dessert Lane, Sweet City, SC 98765</p>
           <iframe
             title="map"
-            src="https://www.google.com/maps/embed?pb=..."
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125261.361642185!2d75.7168993!3d11.2587537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65c1e5ef25bbf%3A0x2d3602d9c964aaf2!2sKozhikode%2C%20Kerala!5e0!3m2!1sen!2sin!4v1716540048452!5m2!1sen!2sin"
             width="100%"
             height="200"
             style={{ border: 0 }}
