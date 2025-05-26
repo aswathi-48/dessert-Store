8f83888b8ad4f5870e5d6a36ba869af570e7d7ad
aswathi-48 <aswathik482@gmail.com>
2025-05-26 12:20:45 +0530

    test image

---
+++ dessert-store/src/Component/test/Images.jsx
@@ -0,0 +1,9 @@
+import img from '../../../public/Images/bg2cake.jpg'
+import img2 from '../../../public/Images/bg3cake.jpg'
+
+const images = {
+    img,
+    img2,
+  };
+  
+  export default images;
---
+++ dessert-store/src/Component/test/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import images from './Images'
+
+const Test = () => {
+  return (
+<div>
+      <img src={images.img} alt="Logo" width={100}/>
+      <img src={images.img2} alt="Data" width={100}/>
+    </div>  )
+}
+
+export default Test
