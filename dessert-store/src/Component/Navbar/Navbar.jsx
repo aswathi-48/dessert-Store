196df36c3bae78716204d6f39016e4ce045f39ed
aswathi-48 <aswathik482@gmail.com>
2025-06-15 20:53:13 +0530

    navbar logo

--- dessert-store/src/Component/Navbar/Navbar.jsx
+++ dessert-store/src/Component/Navbar/Navbar.jsx
@@ -13,7 +13,13 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="logo">Dessert Delight</div>
+      {/* <div className="logo">Dessert Delight</div> */}
+      <div className="logo">
+  <Link to="/">
+    <img src="/Images/logodessert.png" alt="Dessert Delight Logo" className="logo-img" />
+  </Link>
+</div>
+
       <ul className={isMobile ? 'nav-links-mobile' : 'nav-links'} onClick={() => setIsMobile(false)}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
