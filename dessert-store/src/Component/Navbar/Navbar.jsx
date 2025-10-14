7fa2faef6ef309324153b562766f086155382356
aswathi-48 <aswathik482@gmail.com>
2025-10-14 15:46:27 +0530

    navbar

--- dessert-store/src/Component/Navbar/Navbar.jsx
+++ dessert-store/src/Component/Navbar/Navbar.jsx
@@ -8,24 +8,24 @@ const Navbar = () => {
 
   const toggleMenu = () => {
     setIsMobile(!isMobile);
-    
+
   };
 
   return (
     <nav className="navbar">
       {/* <div className="logo">Dessert Delight</div> */}
       <div className="logo">
-  <Link to="/">
-    <img src="/Images/logodessert.png" alt="Dessert Delight Logo" className="logo-img" />
-  </Link>
-</div>
+        <Link to="/">
+          <img src="/Images/logodessert.png" alt="Dessert Delight Logo" className="logo-img" />
+        </Link>
+      </div>
 
       <ul className={isMobile ? 'nav-links-mobile' : 'nav-links'} onClick={() => setIsMobile(false)}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/specials">Specials</Link></li> 
+        <li><Link to="/specials">Specials</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li> 
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
       <button className="mobile-menu-icon" onClick={toggleMenu}>
         {isMobile ? <FaTimes /> : <FaBars />}
